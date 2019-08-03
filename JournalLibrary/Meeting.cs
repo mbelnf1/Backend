47957072f78a24320eb11f71a263f02b90e63238
mbelnf1 <mbeln1992@gmail.com>
2019-08-02 21:12:10 -0400

    Add project files.

---
+++ JournalLibrary/Meeting.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JournalLibrary
+{
+    public class Meeting
+    {
+        public List<Person> PeopleMetWith { get; set; }
+        public DateTime MeetingStartTime { get; set; }
+
+        public DateTime MeetingEndTime { get; set; }
+
+        public string MeetingNotes { get; set; }
+
+        public string Title { get; set; }
+
+        public Meeting()
+        {
+            PeopleMetWith = new List<Person>();
+        }
+    }
+}
